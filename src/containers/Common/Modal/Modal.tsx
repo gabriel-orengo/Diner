41d0b1a33dfc5c214dc9b64de06173d4de4286d5
Gabriel Orengo <gabe.orengo@outlook.com>
2022-04-06 22:08:43 -0400

    Added animation to the Modal component

--- src/containers/Common/Modal/Modal.tsx
+++ src/containers/Common/Modal/Modal.tsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
+/**
+ * Modal component to display a modal
+ * @param props Properties passed into the Modal component
+ * @returns Modal component
+ */
 const Modal = (props: any) => {
-    return (
-        <>
-            <Backdrop action={props.handler} />
-            <div className={classes.Modal}>{props.children}</div>
-        </>
-    );
+	// Animation state
+	const [animation, setAnimation] = useState(0);
+
+	// Get props.display
+	const [display, setDisplay] = useState(props.display);
+
+	// Set animation on display
+	useEffect(() => {
+		display ? setAnimation(1) : setAnimation(0);
+	}, [display]);
+
+	// Function to run closing animation and remove the component
+	const removeComponent = () => {
+		// Run closing animation
+		setDisplay(false);
+
+		// Allows time to run the animation, then removes component
+		setTimeout(props.handler, 400);
+	};
+
+	return (
+		<>
+			<Backdrop action={removeComponent} display={display} />
+			<div className={classes.Modal} data-animation={animation}>
+				{props.children}
+			</div>
+		</>
+	);
 };
 
 export default Modal;
