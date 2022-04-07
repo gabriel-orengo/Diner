39505ee0e42aa36924b1109f76a9c96bc90022d8
Gabriel Orengo <gabe.orengo@outlook.com>
2022-04-06 22:15:49 -0400

    Added paths for Account, Login, and SignUp screens

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,28 @@ import Header from "./containers/Header/Header";
 import Footer from "./containers/Footer/Footer";
 import Content from "./containers/Content/Content";
 import Checkout from "./containers/Checkout/Checkout";
+import Account from "./containers/Account/Account";
+import Login from "./containers/Login/Login";
+import SignUp from "./containers/SignUp/SignUp";
 
 const App = () => {
-    let portal = "User";
+	let portal = "User";
 
-    return (
-        <CartProvider>
-            <div className="App">
-                <Header portal={portal} />
-                <Routes>
-                    <Route path="/" element={<Content portal={portal} />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                </Routes>
-                <Footer />
-            </div>
-        </CartProvider>
-    );
+	return (
+		<CartProvider>
+			<div className="App">
+				<Header portal={portal} />
+				<Routes>
+					<Route path="/" element={<Content portal={portal} />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/account" element={<Account />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+				</Routes>
+				<Footer />
+			</div>
+		</CartProvider>
+	);
 };
 
 export default App;
